d9005ccbf88421ad555a1d08e4e829dd63dc8762
Shenal <shenaldesilva17@gmail.com>
2023-01-26 23:06:41 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -6,8 +7,20 @@ import "../styles/header.css";
 import logo from "../images/logo.png";
 
 function Header() {
+  const [isScrolled, setScrolled] = useState(false);
+
+  const changeNavBar = () => {
+    if (window.scrollY >= 100) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeNavBar);
+
   return (
-    <div className="nav-bar">
+    <div className={isScrolled ? "sticky" : "nav-bar"}>
       <Navbar expand="lg">
         <Container className="header-container">
           <Navbar.Brand href="/">
